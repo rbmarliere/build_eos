5adcb7a09fa4978640f38d8f147acae4d3391564
zaratustra <zrts@protonmail.com>
2018-04-12 23:10:43 -0300

    refator

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Build scripts for EOS.IO software
+# build_eos
 
-```zsh
-./build_all
+This tool is intended to keep it simple the building of EOS.IO software in Funtoo
+
+Sample snippet for including in a shell runcom file (e.g. ~/.bashrc):
+
+```sh
+source ${HOME}/git/build_eos/build_eos.sh
 ```
+
+You can then run build_eos.
 
---
+++ build_eos.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+
+die()
+{
+    [ $# -gt 0 ] && printf -- "%s\n" "$*"
+    return 1
+}
+
+build_boost()
+{
+    BOOST_VER=boost-1.66.0
+
+    WD=${USER_GIT_ROOT}/boost
+    [ ! -d ${WD} ] \
+        && git clone --recursive \
+                     --single-branch \
+                     --branch "${BOOST_VER}" \
+                     git@github.com:boostorg/boost.git \
+                     ${WD}
+    cd ${WD}
+
+    prompt_input_yN "git clean" && git clean -fdx && git checkout .
+
+    mkdir -p {build,release}
+
+    ./bootstrap.sh \
+        --with-toolset=clang
+    ./b2 \
+        --ignore-site-config \
+        -j${NPROC} \
+        --prefix=${WD}/release \
+        toolset=clang \
+        threading=multi \
+        headers
+    ./b2 \
+        --ignore-site-config \
+        -j${NPROC} \
+        --prefix=${WD}/release \
+        toolset=clang \
+        threading=multi \
+        install
+}
+
+build_eos()
+{
+    NPROC=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
+    export NPROC
+
+    prompt_input_yN "build secp256k1-zkp" && build_secp256k1-zkp
+    prompt_input_yN "build boost" && build_boost
+    prompt_input_yN "build eos" || return 0
+
+    if [ "${USER_GIT_ROOT}" = "" ]; then
+        printf "absolute path to clone required projects into, USER_GIT_ROOT="
+        read USER_GIT_ROOT
+    fi
+    case "${USER_GIT_ROOT}" in
+        /*) ;;
+        *) die "error: not an absolute path" ;;
+    esac
+    export USER_GIT_ROOT
+    mkdir -p ${USER_GIT_ROOT}
+
+    WD=${USER_GIT_ROOT}/eos
+    [ ! -d ${WD} ] \
+        && git clone --recursive git@github.com:EOSIO/eos.git ${WD}
+    cd ${WD}
+
+    prompt_input_yN "git clean" && git clean -fdx && git checkout .
+    git fetch --all
+    if prompt_input_yN "checkout specific tag"; then
+        git tag
+        printf "TAG="
+        read TAG
+    fi
+    if prompt_input_yN "git pull"; then
+        git checkout ${TAG}
+        git pull origin ${TAG}
+        git submodule sync
+        git submodule update --init --recursive
+    fi
+    prompt_input_yN "install to system prefix" \
+        || INSTALL_PREFIX="-DCMAKE_INSTALL_PREFIX=${WD}/release"
+
+    mkdir -p {build,release}
+    cd build
+
+    cmake \
+        -DCMAKE_C_COMPILER=/usr/lib/llvm/4/bin/clang \
+        -DBOOST_ROOT=${USER_GIT_ROOT}/boost/release \
+        -DSecp256k1_ROOT_DIR=${USER_GIT_ROOT}/secp256k1-zkp/release \
+        -DLLVM_DIR=/usr/lib/llvm/4/lib64/cmake/llvm \
+        -DWASM_ROOT=/usr/lib/llvm/4 \
+        ${INSTALL_PREFIX} \
+        ..
+    make -j${NPROC} install
+}
+
+build_secp256k1-zkp()
+{
+    WD=${USER_GIT_ROOT}/secp256k1-zkp
+    [ ! -d ${WD} ] \
+        && git clone git@github.com:cryptonomex/secp256k1-zkp.git ${WD}
+    cd ${WD}
+
+    if prompt_input_yN "git clean"; then
+        git clean -fdx
+        git checkout .
+    fi
+
+    ./autogen.sh
+    ./configure --prefix=${WD}/release
+    gmake -j${NPROC} install
+}
+
+#check_deps()
+#{
+#}
+
+prompt_input_yN()
+{
+    printf "${1}? [y|N] " ; shift
+    while true; do
+        read -k 1 yn
+        case ${yn} in
+            [Yy]* ) printf "\n" && return 0; break;;
+            \n ) printf "\n" && return 1; break;;
+            * ) return 1;;
+        esac
+    done
+}
+
